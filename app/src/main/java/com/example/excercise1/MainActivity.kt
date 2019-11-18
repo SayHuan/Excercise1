ce7be40c94d2052498a22a68501ed021c557aa38
TAN SAY HUAN <tansh-jm17@student.tarc.edu.my>
2019-11-18 14:52:20 +0800

    updated

--- app/src/main/java/com/example/excercise1/MainActivity.kt
+++ app/src/main/java/com/example/excercise1/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btncalc.setOnClickListener(){
+            //fucntion call
             calLoan()
         }
     }
